2e9a0c01bee2904482eb8d271c5f93ff5e1e48f0
Antares Chen <antares21chen@gmail.com>
2021-05-19 17:28:49 -0500

    started implementing the bandit algorithm

--- create-input.cpp
+++ create-input.cpp
@@ -1,3 +1,7 @@
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
 using namespace std;
 /***
  * ActionIds:
@@ -8,6 +12,7 @@ using namespace std;
  * ...
  * 
  ***/
+#define NUM_ACTIONS 16
 
 /***
  * DomainIds:
@@ -15,8 +20,55 @@ using namespace std;
  * 1 - Cmp
  * 
  ***/
+#define NUM_DOMAINS 4
 
 
-int gen_input(int crash_reports[], int domain_reports[][4]) {
-    
+// Need x_t, and all mutations on x_t
+
+// Everytime this is called, do one iteration of multi-armed bandits
+// Write file into queue location
+// Return 0 if successs, or 1 otherwise
+int gen_input(char crash_reports[], int domain_reports[NUM_ACTIONS][NUM_DOMAINS]) {
+    // Compute advice
+    // Compute unmixed probabilities
+    // Call mix_prob
+    // Sample action
+    // Compute reward vector
+    // Compute reward estimator
+    // Compute weight update
+    // Add x_t to seen set
+    // Update with new input
 }
+
+// Returns the aggregate score from the domain_reports
+// Create file, call add_to_queue(filename, length, something else that's 0) 
+int get_advice(int domain_reports[NUM_ACTIONS][NUM_DOMAINS], std::vector<double> &advice) {
+    // TODO: implement me
+    return 0;
+}
+
+// Algorithm 2 in the paper
+int mix_prob(const double p_min, std::vector<double> &prob) {
+    // Rescaling variables
+    double delta = 0;
+    double s = 0;
+
+    // Get indices of prob sorted by magnitude of prob
+    std::vector<size_t> indices(prob.size());
+    iota(indices.begin(), indices.end(), 0);
+    stable_sort(indices.begin(), indices.end(), 
+        [&prob](size_t a, size_t b){return prob[a] < prob[b]});
+
+
+    // Iterate through the action probabilities in sorted order
+    // For actions j=1...K in the order of prob.get(j)
+        // if (p_j * (1 - delta / s)) >= p_min
+            // For all remaining actions p_j'
+                // Set p_j' = p_j' * (1 - delta/s)
+        // else
+            // p_j_old = p_j
+            // p_j = p_min
+            // delta = delta + p_j - p_j_old
+            // s = s - p_j_old 
+}
+
