ff3767348e910c51671ebefd99f8113ca2d544e0
Joshua Viszlai <viszlai@uchicago.edu>
2021-05-19 16:01:51 -0500

    structure of algorithm

---
+++ create-input.cpp
@@ -0,0 +1,22 @@
+using namespace std;
+/***
+ * ActionIds:
+ * 0 - 
+ * 1 - 
+ * 2 - 
+ * 3 -
+ * ...
+ * 
+ ***/
+
+/***
+ * DomainIds:
+ * 0 - Mem
+ * 1 - Cmp
+ * 
+ ***/
+
+
+int gen_input(int crash_reports[], int domain_reports[][4]) {
+    
+}
