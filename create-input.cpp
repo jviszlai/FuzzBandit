99e2a23a30a5bac29ba7e41c4edba603ab6f6c04
Joshua Viszlai <viszlai@uchicago.edu>
2021-05-22 13:06:10 -0500

    add logging ingo

--- create-input.cpp
+++ create-input.cpp
@@ -4,6 +4,9 @@
 #include <cmath>
 #include <numeric>
 #include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <iterator>
 
 using namespace std;
 
@@ -26,7 +29,7 @@ using namespace std;
  * 14 -
  * 15 -
  ***/
-#define NUM_ACTIONS 1166
+#define NUM_ACTIONS 1137
 
 /***
  * DomainIds:
@@ -46,17 +49,20 @@ using namespace std;
 std::map<unsigned int, double> weight_map;
 std::set<unsigned int> visited_hashes;
 
+static ofstream log_file;
+static std::string log_file_name = "prob_log.txt";
+
 // Global array of weights on the domain
 double domain_weights[] = {1};
 
 // C API
 extern "C"
 {
-    int gen_input(char crash_reports[], int domain_reports[NUM_ACTIONS][NUM_DOMAINS], unsigned int curr_hash, unsigned int mutated_hash[NUM_ACTIONS]);
+    int gen_input(char crash_reports[], int (*domain_reports)[NUM_ACTIONS][NUM_DOMAINS], unsigned int curr_hash, unsigned int mutated_hash[NUM_ACTIONS]);
 }
 
 // Function declarations
-int get_advice(int domain_reports[NUM_ACTIONS][NUM_DOMAINS], std::vector<double> &advice);
+int get_advice(int (*domain_reports)[NUM_ACTIONS][NUM_DOMAINS], std::vector<double> &advice);
 int get_mix_prob(const double p_min, std::vector<double> &prob);
 int sample_mutation(const double total_prob, const std::vector<double> &prob);
 
@@ -77,8 +83,16 @@ private:
 // Everytime this is called, do one iteration of multi-armed bandits
 // Write file into queue location
 // Return 0 if successs, or 1 otherwise
-int gen_input(char crash_reports[], int domain_reports[NUM_ACTIONS][NUM_DOMAINS], unsigned int curr_hash, unsigned int mutated_hash[NUM_ACTIONS])
+int gen_input(char crash_reports[], int (*domain_reports)[NUM_ACTIONS][NUM_DOMAINS], unsigned int curr_hash, unsigned int mutated_hash[NUM_ACTIONS])
 {
+    log_file.open(log_file_name, std::ios_base::app);
+
+
+    log_file << "================================\n\n\n REPORTS \n\n\n\ ";
+    for (int i = 0; i < 100; i++) {
+        log_file << (*domain_reports)[i][1] << ", ";
+    }
+
     // Compute advice
     std::vector<double> advice(NUM_ACTIONS);
     get_advice(domain_reports, advice);
@@ -122,12 +136,30 @@ int gen_input(char crash_reports[], int domain_reports[NUM_ACTIONS][NUM_DOMAINS]
     // Add x_t to the seen set
     visited_hashes.insert(curr_hash);
 
+    std::ostringstream oss;
+    log_file << "\n\n\n PROB \n\n\n\ ";
+    std::copy(prob.begin(), prob.end()-1000,
+        std::ostream_iterator<double>(oss, ", "));
+
+    log_file << oss.str();
+
+    std::ostringstream oss2;
+    log_file << "\n\n\n ADVICE \n\n\n\ ";
+    std::copy(advice.begin(), advice.end()-1000,
+        std::ostream_iterator<double>(oss2, ", "));
+
+    log_file << oss2.str();
+    log_file << "\n\n MUTATION \n\n";
+    log_file << mutation;
+
+
+    log_file.close();
     // Output sampled mutation
     return mutation;
 }
 
 // Returns the aggregate score from the domain_reports
-int get_advice(int domain_reports[NUM_ACTIONS][NUM_DOMAINS], std::vector<double> &advice)
+int get_advice(int (*domain_reports)[NUM_ACTIONS][NUM_DOMAINS], std::vector<double> &advice)
 {
     // Compute aggregate score for each action
     double total_score = 0;
@@ -136,7 +168,13 @@ int get_advice(int domain_reports[NUM_ACTIONS][NUM_DOMAINS], std::vector<double>
         double aggregate_score = 0;
         for (int j = 0; j < NUM_DOMAINS; j++)
         {
-            aggregate_score += domain_weights[j] * domain_reports[i][j];
+            aggregate_score += domain_weights[j] * (*domain_reports)[i][j];
+            if ((*domain_reports)[i][j] < 0) {
+                log_file << "\n\nBIG MEME WHY :(\n\n" << (*domain_reports)[i][j];
+            }
+        }
+        if (aggregate_score < 0) {
+            log_file << "\n\nAGGREGATE BIG MEME WHY :(\n\n" << aggregate_score;
         }
         advice[i] = aggregate_score;
         total_score += aggregate_score;
